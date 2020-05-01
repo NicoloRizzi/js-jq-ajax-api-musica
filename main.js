5a2a3f94832841771e89b32e9a93f1b59a7eb2ca
Nicolo <nicolorizzi1996@gmail.com>
2020-05-01 12:32:45 +0200

    FUNCTION ADDED ON CHANGED VALUE IN A SELECT ELEMENT

--- main.js
+++ main.js
@@ -5,30 +5,46 @@ Servendoci di handlebars stampiamo tutto a schermo.
 In questo momento non è importante la parte grafica.
 Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
  */
-$(document).ready(function() {
+$(document).ready(function () {
 	var container = $('.cds-container');
 	var source = $("#entry-template").html();
 	var template = Handlebars.compile(source);
-		$.ajax({
-			type: "GET",
-			url: "https://flynn.boolean.careers/exercises/api/array/music",
-			success: function (data) {
-				var cd = data.response;				
-				for(var i=0; i < cd.length; i++) {
-					var element = cd[i];
-					var info = {
-						imgSrc: element.poster,
-						title: element.title,
-						subtitle: element.author,
-						yearCd: element.year
-					}
-					var html = template(info);
-					container.append(html)
-					
+
+	$.ajax({
+		type: "GET",
+		url: "https://flynn.boolean.careers/exercises/api/array/music",
+		success: function (data) {
+			var cd = data.response;
+			for (var i = 0; i < cd.length; i++) {
+				var element = cd[i];
+
+
+				var info = {
+					imgSrc: element.poster,
+					title: element.title,
+					subtitle: element.author,
+					yearCd: element.year,
+					genres: element.genre.toLowerCase()
 				}
-			},
-			error: function () {
-				console.error('ERROR');
+
+				var html = template(info);
+				container.append(html);
+
 			}
-		});
+		},
+		error: function () {
+			console.error('ERROR');
+		}
+	});// END AJAX
+	$('#genres').change(function () {
+		console.log($(this).val());
+		
+	var genre = $(this).val();
+	if (genre === 'all') {
+		$('.cd').show();
+	} else {
+		$('.cd').hide();
+		$('.cd.' + genre).show();
+	}
+}); // END CHANGE*/
 }); //END DOC READY
